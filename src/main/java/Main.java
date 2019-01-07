64da2b706f36450584bd856e8b1909b8d6e454be
theogutt <theodorguttesen@gmail.com>
2019-01-07 14:08:10 +0100

    Tilføjede GUI spilleplade og MVC pakker

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+import View.GUI_Handler;
+
 public class Main {
     public static void main(String[]args){
-
+        GUI_Handler gui_handler = new GUI_Handler();
+        gui_handler.startGUI();
     }
 }
