e8f2aa75f7513067a5d60bc979b2461bfbee5b72
Andreas <mail.Tailspot@gmail.com>
2019-01-09 15:46:51 +0100

    Start klasse -Andreas

--- src/main/java/Model/Squares/Start.java
+++ src/main/java/Model/Squares/Start.java
@@ -1,4 +1,25 @@
 package Model.Squares;
 
-public class Start {
+import Controller.PlayerController;
+
+public class Start extends Square{
+
+    //Constructor
+    public Start(int positionOnBoard){
+        super(positionOnBoard);
+    }
+
+    @Override
+    public int getPositionOnBoard(){
+        return super.getPositionOnBoard();
+    }
+
+    @Override
+    public int landOn(PlayerController playerNum){
+        /*
+        Retunere værdi som en controller bruger
+        til at finde ud af hvad den skal gøre
+        */
+        return -1;
+    }
 }
