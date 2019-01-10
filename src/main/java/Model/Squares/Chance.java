b406af6ac05bd46762079d0305505204fcf778a2
Andreas <mail.Tailspot@gmail.com>
2019-01-10 11:33:36 +0100

    Chance klasse -Andreas

--- src/main/java/Model/Squares/Chance.java
+++ src/main/java/Model/Squares/Chance.java
@@ -1,4 +1,23 @@
 package Model.Squares;
 
-public class Chance {
+import Controller.PlayerController;
+
+public class Chance extends Square{
+
+    public Chance(int positionOnBoard) {
+        super(positionOnBoard);
+    }
+
+    @Override
+    public int getPositionOnBoard(){
+        return super.getPositionOnBoard();
+    }
+
+    @Override
+    public int landOn(PlayerController playerNum){
+
+        //Retur værdi som en controller skal bruge til at finde ud af hvad den skal gøre
+        //Selve værdien kan sagtens ændres
+        return -3;
+    }
 }
