dc5936e84b4601a00798bb2d89e7d2696399d57f
Jonatan <s185096@student.dtu.dk>
2019-01-10 15:09:13 +0100

    Startet på ChanceController

---
+++ src/main/java/Controller/ChanceController.java
@@ -0,0 +1,20 @@
+package Controller;
+import Controller.GameBoard;
+
+public class ChanceController {
+    private int[] chanceCards = new int[20];
+    private int positionOnBoard;
+
+    public ChanceController(int positionOnBoard) {
+        this.positionOnBoard = positionOnBoard;
+
+    }
+
+    public int landedOnChance(PlayerController playerC, int playerNum){
+        int card = chanceCards[0];
+
+
+
+    }
+
+}
