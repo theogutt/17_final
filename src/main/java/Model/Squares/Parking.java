186cff2ce49efb4967e0c493f0550c9df413f985
Andreas <mail.Tailspot@gmail.com>
2019-01-10 10:52:26 +0100

    Parking klasse -Andreas

--- src/main/java/Model/Squares/Parking.java
+++ src/main/java/Model/Squares/Parking.java
@@ -1,4 +1,23 @@
 package Model.Squares;
 
-public class Parking {
+import Controller.PlayerController;
+
+public class Parking extends Square{
+
+    public Parking(int positionOnBoard){
+        super(positionOnBoard);
+    }
+
+    @Override
+    public int getPositionOnBoard(){
+        return super.getPositionOnBoard();
+    }
+
+    @Override
+    public int landOn(PlayerController playerNum){
+
+        //Retur værdi som en controller skal bruge til at finde ud af hvad den skal gøre
+        //Selve værdien kan sagtens ændres
+        return -1;
+    }
 }
