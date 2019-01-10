3d59baec1eb52924ca09f538bf4e04d512467149
Your Name <you@example.com>
2019-01-10 14:12:06 +0100

    Brygerier leje og færge leje

--- src/main/java/Model/Squares/Ferry.java
+++ src/main/java/Model/Squares/Ferry.java
@@ -8,6 +8,12 @@ public class Ferry extends Ownable{
         super(positionOnBoard, price, rent, owned);
     }
 
+// Ændrer leje i forhold til antal færger
+    public void changeRent(int numbersOfOwnedFerrys){
+        this.rent = (int)(250 * Math.pow(2,numbersOfOwnedFerrys));
+    }
+
+
     @Override
     public int landOn(PlayerController playerNum) {
         return 0;
