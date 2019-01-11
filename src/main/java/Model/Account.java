22dbad436bb4f14e4892f3c0ec8e8ffbfdfd92e7
Your Name <you@example.com>
2019-01-11 14:40:52 +0100

    Tilføj ownable og remove ownable

--- src/main/java/Model/Account.java
+++ src/main/java/Model/Account.java
@@ -1,29 +1,57 @@
 package Model;
-import java.util.ArrayList;
 
-//import Model.Square.Street;
-//import Model.Square.Square;
+import java.util.Arrays;
+
+import Model.Squares.Ownable;
 
 
 public class Account{
     private int balance;
     private int sumOfStreets;
-/*
-    private ArrayList <Square> playersStreets = new ArrayList <Square>();
-*/
+    private Ownable [] playerOwnables = new Ownable[0];
+
+    // private ArrayList <Square> playersStreets = new ArrayList <Square>();
 
     public Account(){
         startAccount();
+    }
+
+    public void startAccount(){
+        balance = 30000;
+    }
+
+    //tilføjer et Ownable til playerownables
+    public void addOwnable(Ownable ownable){
+        Ownable [] newArray = copyOf(this.playerOwnables,this.playerOwnables.length+1);
+        newArray[this.playerOwnables.length] = ownable;
+        this.playerOwnables = newArray;
+    }
+    public void removeOwnable(Ownable ownable){
+        int x = 0;
+
+        //Tjekker hvor grunden, der skal fernes er
+        for(int i= 0;i < this.playerOwnables.length; i++){
+            if (this.playerOwnables[i] == ownable){
+                x=i;
+                break;
+            }
+        }
+        // Sætter alt på den nye plads i Arrayet
+        for(int i = x; x < this.playerOwnables.length; i++){
+            this.playerOwnables[x] = this.playerOwnables[x+1];
+        }
+        copyOf(this.playerOwnables,this.playerOwnables.length-1);
+    }
+
+    public static Model.Squares.Ownable[] copyOf(Model.Squares.Ownable[] original, int newLength){
+        Ownable [] newArray = copyOf(original,newLength);
+        return newArray;
     }
 
    /* public void addStreet(Street street){
         playersStreets.add(street);
         sumOfStreets += street.getPrice();
     }*/
-
-    public void startAccount(){
-        balance = 30000;
-    }
 
    /* public boolean ownsStreetAheadOrBehind(int lfStreetPosition){
         boolean x = false;
