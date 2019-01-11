ed598a61b223d3c7542b2f3944a1c483254f8599
Andreas <mail.Tailspot@gmail.com>
2019-01-11 13:19:10 +0100

    Redigeret i landOn metoder -Andreas

--- src/main/java/Model/Squares/Ferry.java
+++ src/main/java/Model/Squares/Ferry.java
@@ -16,7 +16,7 @@ public class Ferry extends Ownable{
     }
 
     @Override
-    public int landOn(PlayerController playerC, int positionOnBoard, int faceValueSum){
+    public int landOn(PlayerController playerC, int positionOnBoard, int faceValueSum, int ref){
         return -1;
     }
 }
--- src/main/java/Model/Squares/Square.java
+++ src/main/java/Model/Squares/Square.java
@@ -15,5 +15,5 @@ public abstract class Square {
         return positionOnBoard;
     }
 
-    public abstract int landOn(PlayerController playerC, int positionOnBoard, int faceValueSum);
+    public abstract int landOn(PlayerController playerC, int positionOnBoard, int faceValueSum, int ref);
 }
