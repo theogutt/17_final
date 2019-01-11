2388509f8acd3913de9ca740ead0018bbbe99093
Your Name <you@example.com>
2019-01-11 10:38:24 +0100

    Færdiggjort instantiateSquares

--- src/main/java/Controller/GameBoard.java
+++ src/main/java/Controller/GameBoard.java
@@ -7,25 +7,26 @@ import java.util.HashMap;
 public class GameBoard {
 
     private Square[] squares = new Square[40];
-    private PlayerController PlayerC = new PlayerController();
-    private Square Square = new Square();
+    private PlayerController PlayerC;
     private HashMap squarePrice;
+    private HashMap BaseRent;
 
-
+// Opretter felter med pris og rent
     public void instantiateSquares(){
         for (int i = 0; i < 40; i++) {
-            if (i == 0){ squares[i]= new Start(i);}                                                                 //Start
-            else if (i == 10 || i == 20 ){ squares[i]= new Parking(i);}                                              //Parking
-            else if (i == 30 ){ squares[i]= new GoToPrison(i);}                                                      //Jail
-            else if (i == 2 || i == 7 || i == 17 || i == 22 || i == 33 || i == 36) {squares[i]= new Chance(i);}     //Chance
-            else if (i == 4 || i == 38){squares[i]= new Tax(i);}                                                    //Tax
-            else{squares[i]= new Street(i, (Integer)squarePrice.get(i),false);                                      //Street
+            if (i == 0){ squares[i]= new Start(i);}                                                                        //Start
+            else if (i == 10 || i == 20 ){ squares[i]= new Parking(i);}                                                    //Parking
+            else if (i == 30 ){ squares[i]= new GoToPrison(i);}                                                            //Jail
+            else if (i == 2 || i == 7 || i == 17 || i == 22 || i == 33 || i == 36) {squares[i]= new Chance(i);}            //Chance
+            else if (i == 4 || i == 38){squares[i]= new Tax(i);}                                                           //Tax
+            else if (i == 6 || i == 16 ||i == 26 ||i == 36) {squares[i] = new Ferry(i,4000,500,false);} //Ferry
+            else if (i == 13 || i == 29){squares[i] = new Brewery(i,3000,111,false);}                   //Brew
+            else {squares[i]= new Street(i,(Integer)squarePrice.get(i),(Integer)BaseRent.get(i),false);            //Street
             }
         }
     }
 
-
-
+/*
     public void landOnEffect(int playerNum){
 
         // i er den reelle spiller position på brættet
@@ -34,6 +35,7 @@ public class GameBoard {
 
         //Start felt
         if (i == 1){
+
             PlayerC.updatePlayerBalance(playerNum, 4000);
         }
 
@@ -69,5 +71,5 @@ public class GameBoard {
 
         }
     }
-
+    */
 }
