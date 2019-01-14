f6cd2c13d896370dc9c6b0a5d2741f6203ed7f1a
Andreas <mail.Tailspot@gmail.com>
2019-01-14 14:34:31 +0100

    Genindsat GoToPrison klasse -Andreas

--- src/main/java/Model/Squares/GoToPrison.java
+++ src/main/java/Model/Squares/GoToPrison.java
@@ -4,13 +4,22 @@ import Controller.PlayerController;
 import Controller.RentController;
 import View.GUI_Handler;
 
+
+
 public class GoToPrison extends Square{
+
+    private PlayerController playerC;
+
     public GoToPrison(int positionOnBoard) {
         super(positionOnBoard);
     }
 
     @Override
-    public int landOn(PlayerController playerC, int ref, GUI_Handler guiHandler, RentController rentC) {
-        return super.landOn(playerC, ref, guiHandler, rentC);
+    public int landOn(PlayerController playerC, int playerNum, GUI_Handler guiHandler, RentController rentC) {
+
+        int p = playerC.getPlayerPlayerNum(playerNum);
+        playerC.setPosition(10,p);
+        playerC.setInJail(p, true);
+        return -2;
     }
 }
