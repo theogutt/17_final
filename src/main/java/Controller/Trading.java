b68198aa3c42fedc41f680c9d3f30947910721fd
aleksanderlj <aleksanderlj@hotmail.com>
2019-01-14 14:20:19 +0100

    Trading constructor slettet //Aleks

--- src/main/java/Controller/Trading.java
+++ src/main/java/Controller/Trading.java
@@ -4,21 +4,10 @@ import Model.Player;
 import Model.Squares.Ownable;
 
 public class Trading {
-    Ownable[] curPlayerOwnables; //SLET DISSE
-    Ownable[] othPlayerOwnables;
-
-    public Trading(PlayerController playerC, int curRef, int othRef){
-        this.curPlayerOwnables = playerC.getPlayerOwnables(curRef);
-        this.othPlayerOwnables = playerC.getPlayerOwnables(othRef);
-    }
-
     public void trade(PlayerController playerC, int curRef, Ownable curOwnable, int curMoney,
                                                 int othRef, Ownable othOwnable, int othMoney){
 
         // Ownable overførsel
-
-
-
         if (othOwnable != null){
             playerC.removeOwnable(othOwnable, othRef);
             playerC.addOwnable(othOwnable, curRef);
@@ -28,7 +17,6 @@ public class Trading {
             playerC.removeOwnable(curOwnable, curRef);
             playerC.addOwnable(curOwnable, othRef);
         }
-
 
         // Penge overførsel
         playerC.updatePlayerBalance(curRef, -curMoney);
