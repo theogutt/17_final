5b1c2071cc53264370c2f383e82b293d81e56979
aleksanderlj <aleksanderlj@hotmail.com>
2019-01-14 17:21:24 +0100

    Cleanup af Ownable (og Square) //Aleks

--- src/main/java/Model/Squares/Ferry.java
+++ src/main/java/Model/Squares/Ferry.java
@@ -10,32 +10,19 @@ public class Ferry extends Ownable{
     private int owner;
     private int price;
     private boolean owned;
-    private int positionOnBoard;
     private String name;
-    private GUI_Handler guiHandler;
-    private PlayerController playerC;
-    private RentController rentC;
 
     public Ferry(int positionOnBoard, int price, String name, boolean owned, int owner) throws IOException {
-        super(positionOnBoard);
-        this.price = price;
-        this.owned = owned;
-        this.name = name;
-        this.owner = owner;
-    }
-
-    public int getPrice() {
-        return price;
+        super(positionOnBoard, price, name, owned, owner);
     }
 
     public int landOn(PlayerController playerC, int ref, GUI_Handler guiHandler, RentController rentC) {
         //spørger om spiller vil købe grunden
-        if(isOwned()==false) {
+        if(!isOwned()) {
             int ja = 1;
             int answer = guiHandler.buyStreet();
             if (ja == answer) {
-                int price = getPrice(positionOnBoard);
-                playerC.updatePlayerBalance(ref, price * -1);
+                playerC.updatePlayerBalance(ref, this.price * -1);
                 setOwner(ref);
                 setOwned(true);
             } else {}
@@ -45,7 +32,7 @@ public class Ferry extends Ownable{
         }
         return -1;
     }
-    public int getPrice(int positionOnBoard) {
+    public int getPrice() {
         return price;
     }
 
@@ -53,9 +40,6 @@ public class Ferry extends Ownable{
         return name;
     }
 
-    public int getPositionOnBoard() {
-        return positionOnBoard;
-    }
     public int getOwner() {
         return this.owner;
     }
--- src/main/java/Model/Squares/Ownable.java
+++ src/main/java/Model/Squares/Ownable.java
@@ -5,14 +5,23 @@ import Controller.RentController;
 import View.GUI_Handler;
 
 public abstract class Ownable extends Square{
-    public Ownable(int positionOnBoard) {
+    private int owner;
+    private int price;
+    private boolean owned;
+    private int positionOnBoard;
+    private String name;
+
+    public Ownable(int positionOnBoard, int price, String name, boolean owned, int owner) {
         super(positionOnBoard);
+        this.price = price;
+        this.owned = owned;
+        this.name = name;
+        this.owner = owner;
     }
 
     public abstract int landOn(PlayerController playerC, int ref, GUI_Handler guiHandler, RentController rentC);
 
-
     public int getOwner() {
-        return getOwner();
+        return owner;
     }
 }
