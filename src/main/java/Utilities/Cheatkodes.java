c77c3a710748a5c1c15d3c3d379cc0cd4382a21e
Andreas <mail.Tailspot@gmail.com>
2019-01-15 14:50:06 +0100

    Lavet en cheatkodes klasse -Andreas

---
+++ src/main/java/Utilities/Cheatkodes.java
@@ -0,0 +1,46 @@
+package Utilities;
+
+import Controller.GameEngine;
+import Controller.PlayerController;
+import Controller.RentController;
+import Model.Squares.Square;
+import View.GUI_Handler;
+
+import java.util.Scanner;
+
+
+
+public class Cheatkodes {
+
+    private Square[] squares = new Square[40];
+
+    public void cheats(PlayerController playerC, int playerNum, GameEngine gameEngine, GUI_Handler guiHandler, RentController rentC){
+
+        Scanner scan = new Scanner(System.in);
+        int input = scan.nextInt();
+
+        if (input == 1){
+            playerC.setJailCard(playerNum, true);
+        }
+        else if(input == 2){
+            playerC.setInJail(playerNum, true);
+            playerC.setPosition(10, playerNum);
+        }
+        else if(input == 3){
+            playerC.updatePlayerBalance(playerNum, -50000);
+        }
+        else if(input == 4){
+            playerC.updatePlayerBalance(playerNum, 50000);
+        }
+        else if(input == 5){
+            gameEngine.setPairs(3);
+        }
+        else if(input == 6){
+            gameEngine.setPairs(1);
+        }
+        else if(input == 7){
+            playerC.setPosition(4, playerNum);
+            squares[4].landOn(playerC,playerNum,guiHandler,rentC);
+        }
+    }
+}
