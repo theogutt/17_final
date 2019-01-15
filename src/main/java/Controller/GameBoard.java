ad4236a45ea2ab3ce4cb7af27ee3bc48efd5c39a
theogutt <theodorguttesen@gmail.com>
2019-01-15 15:49:19 +0100

    rykket landon til ownable

--- src/main/java/Controller/GameBoard.java
+++ src/main/java/Controller/GameBoard.java
@@ -55,9 +55,9 @@ public class GameBoard {
             else if (i == 30 ){ squares[i]= new GoToPrison(i);}                                                            //Jail
             else if (i == 2 || i == 7 || i == 17 || i == 22 || i == 33 || i == 36) {squares[i]= new Chance(i);}            //Chance
             else if (i == 4 || i == 38){squares[i]= new Tax(i);}                                                           //Tax
-            else if (i == 5 || i == 15 ||i == 25 ||i == 35) {squares[i] = new Ferry(i,4000,(String)StreetName.get(i), false, 99, (Integer)groupID.get(i));} //Ferry
-            else if (i == 12 || i == 27){squares[i] = new Brewery(i,3000,(String)StreetName.get(i), false, 99, (Integer)groupID.get(i));}            //Brew
-            else {squares[i]= new Street(i,(Integer)squarePrice.get(i),0,(String)StreetName.get(i),(Integer)groupID.get(i),false, 99); //Street
+            else if (i == 5 || i == 15 ||i == 25 ||i == 35) {squares[i] = new Ferry(i,4000,0,(String)StreetName.get(i),(Integer)groupID.get(i), false, 98);} //Ferry
+            else if (i == 12 || i == 27){squares[i] = new Brewery(i,3000,0,(String)StreetName.get(i), (Integer)groupID.get(i), false, 98);}            //Brew
+            else {squares[i]= new Street(i,(Integer)squarePrice.get(i),0,(String)StreetName.get(i),(Integer)groupID.get(i),false, 98); //Street
             }
         }
     }
