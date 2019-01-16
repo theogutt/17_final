b99ae429ab666c424640ac40473ab0b1414ed22e
Andreas <mail.Tailspot@gmail.com>
2019-01-16 16:09:23 +0100

    Merge development ind i cheatkodes -Andreas

--- src/main/java/Controller/GameEngine.java
+++ src/main/java/Controller/GameEngine.java
@@ -118,7 +118,7 @@ public class GameEngine {
                 playerC.setInJail(playerNum, true);
             }
             else{
-                playTurn(playerNum);
+                playTurn(playerNum, cheatkodes);
             }
         }
     }
