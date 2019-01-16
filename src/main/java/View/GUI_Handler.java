b191a1c9f87ff4d72ee1138dcfa10600cf8069c2
aleksanderlj <aleksanderlj@hotmail.com>
2019-01-16 23:47:48 +0100

    Trading færdig, bortset for én bug //Aleks

--- src/main/java/View/GUI_Handler.java
+++ src/main/java/View/GUI_Handler.java
@@ -22,7 +22,7 @@ import static gui_fields.GUI_Car.Type.*;
 public class GUI_Handler {
     private GUI gui;
     private MessageHandler message;
-    private static GUI_Field[] fields;
+    private GUI_Field[] fields;
     private GUI_Player[] gui_Players;
     private GUI_Car[] gui_cars;
     private HashMap chanceDesc;
@@ -250,158 +250,159 @@ public class GUI_Handler {
         ownable.setBorder(carColor);
     }
 
-    public void setSpecificFields(){
+    public void setSpecificFields() throws IOException{
+        HashMap StreetName = TextReader.textReader(".\\src\\Resources\\StreetName");
             //ejendomsfelter
             GUI_Street rødovrevej = new GUI_Street();
             fields[1] = rødovrevej;
-            rødovrevej.setTitle("Rødovrevej");
+            rødovrevej.setTitle((String)StreetName.get(1));
             rødovrevej.setSubText("kr. 1.200");
             rødovrevej.setBorder(Color.BLACK);
             rødovrevej.setBackGroundColor(Color.cyan);
 
             GUI_Street hvidovrevej = new GUI_Street();
             fields[3] = hvidovrevej;
-            hvidovrevej.setTitle("Hvidovrevej");
+            hvidovrevej.setTitle((String)StreetName.get(3));
             hvidovrevej.setSubText("kr. 1.200");
             hvidovrevej.setBorder(Color.BLACK);
             hvidovrevej.setBackGroundColor(Color.cyan);
 
             GUI_Street roskildevej = new GUI_Street();
             fields[6] = roskildevej;
-            roskildevej.setTitle("Roskildevej");
+            roskildevej.setTitle((String)StreetName.get(6));
             roskildevej.setSubText("kr. 2.000");
             roskildevej.setBorder(Color.BLACK);
             roskildevej.setBackGroundColor(Color.pink);
 
             GUI_Street valby_Langgade = new GUI_Street();
             fields[8] = valby_Langgade;
-            valby_Langgade.setTitle("Valby Langgade");
+            valby_Langgade.setTitle((String)StreetName.get(8));
             valby_Langgade.setSubText("kr. 2.000");
             valby_Langgade.setBorder(Color.BLACK);
             valby_Langgade.setBackGroundColor(Color.pink);
 
             GUI_Street allégade = new GUI_Street();
             fields[9] = allégade;
-            allégade.setTitle("Allégade");
+            allégade.setTitle((String)StreetName.get(9));
             allégade.setSubText("kr. 2.400");
             allégade.setBorder(Color.BLACK);
             allégade.setBackGroundColor(Color.pink);
 
             GUI_Street frederiksberg_Allé = new GUI_Street();
             fields[11] = frederiksberg_Allé;
-            frederiksberg_Allé.setTitle("Frederiksberg Allé");
+            frederiksberg_Allé.setTitle((String)StreetName.get(11));
             frederiksberg_Allé.setSubText("kr. 2.800");
             frederiksberg_Allé.setBorder(Color.BLACK);
             frederiksberg_Allé.setBackGroundColor(new Color(173, 255, 47));
 
             GUI_Street bülowsvej = new GUI_Street();
             fields[13] = bülowsvej;
-            bülowsvej.setTitle("Bülowsvej");
+            bülowsvej.setTitle((String)StreetName.get(13));
             bülowsvej.setSubText("kr. 2.800");
             bülowsvej.setBorder(Color.BLACK);
             bülowsvej.setBackGroundColor(new Color(173, 255, 47));
 
             GUI_Street gl_Kongevej = new GUI_Street();
             fields[14] = gl_Kongevej;
-            gl_Kongevej.setTitle("Gl. Kongevej");
+            gl_Kongevej.setTitle((String)StreetName.get(14));
             gl_Kongevej.setSubText("kr. 3.200");
             gl_Kongevej.setBorder(Color.BLACK);
             gl_Kongevej.setBackGroundColor(new Color(173, 255, 47));
 
             GUI_Street bernstorffsvej = new GUI_Street();
             fields[16] = bernstorffsvej;
-            bernstorffsvej.setTitle("Bernstorffsvej");
+            bernstorffsvej.setTitle((String)StreetName.get(16));
             bernstorffsvej.setSubText("kr. 3.600");
             bernstorffsvej.setBorder(Color.BLACK);
             bernstorffsvej.setBackGroundColor(Color.gray);
 
             GUI_Street hellerupvej = new GUI_Street();
             fields[18] = hellerupvej;
-            hellerupvej.setTitle("Hellerupvej");
+            hellerupvej.setTitle((String)StreetName.get(18));
             hellerupvej.setSubText("kr. 3.600");
             hellerupvej.setBorder(Color.BLACK);
             hellerupvej.setBackGroundColor(Color.gray);
 
             GUI_Street strandvej = new GUI_Street();
             fields[19] = strandvej;
-            strandvej.setTitle("Strandvej");
+            strandvej.setTitle((String)StreetName.get(19));
             strandvej.setSubText("kr. 4.000");
             strandvej.setBorder(Color.BLACK);
             strandvej.setBackGroundColor(Color.gray);
 
             GUI_Street trianglen = new GUI_Street();
             fields[21] = trianglen;
-            trianglen.setTitle("Trianglen");
+            trianglen.setTitle((String)StreetName.get(21));
             trianglen.setSubText("kr. 4.400");
             trianglen.setBorder(Color.BLACK);
             trianglen.setBackGroundColor(Color.red);
 
             GUI_Street østerbrogade = new GUI_Street();
             fields[23] = østerbrogade;
-            østerbrogade.setTitle("Østerbrogade");
+            østerbrogade.setTitle((String)StreetName.get(23));
             østerbrogade.setSubText("kr. 4.400");
             østerbrogade.setBorder(Color.BLACK);
             østerbrogade.setBackGroundColor(Color.red);
 
             GUI_Street grønningen = new GUI_Street();
             fields[24] = grønningen;
-            grønningen.setTitle("Grønningen");
+            grønningen.setTitle((String)StreetName.get(24));
             grønningen.setSubText("kr. 4.800");
             grønningen.setBorder(Color.BLACK);
             grønningen.setBackGroundColor(Color.red);
 
             GUI_Street bredgade = new GUI_Street();
             fields[26] = bredgade;
-            bredgade.setTitle("Bredgade");
+            bredgade.setTitle((String)StreetName.get(26));
             bredgade.setSubText("kr. 5.200");
             bredgade.setBorder(Color.BLACK);
             bredgade.setBackGroundColor(Color.white);
 
             GUI_Street kgs_nytorv = new GUI_Street();
             fields[27] = kgs_nytorv;
-            kgs_nytorv.setTitle("Kgs. nytorv");
+            kgs_nytorv.setTitle((String)StreetName.get(27));
             kgs_nytorv.setSubText("kr. 5.200");
             kgs_nytorv.setBorder(Color.BLACK);
             kgs_nytorv.setBackGroundColor(Color.white);
 
             GUI_Street østergade = new GUI_Street();
             fields[29] = østergade;
-            østergade.setTitle("Østergade");
+            østergade.setTitle((String)StreetName.get(29));
             østergade.setSubText("kr. 5.600");
             østergade.setBorder(Color.BLACK);
             østergade.setBackGroundColor(Color.white);
 
             GUI_Street amagertorv = new GUI_Street();
             fields[31] = amagertorv;
-            amagertorv.setTitle("Amagertorv");
+            amagertorv.setTitle((String)StreetName.get(31));
             amagertorv.setSubText("kr. 6.000");
             amagertorv.setBorder(Color.BLACK);
             amagertorv.setBackGroundColor(Color.yellow);
 
             GUI_Street vimmelskaftet = new GUI_Street();
             fields[32] = vimmelskaftet;
-            vimmelskaftet.setTitle("Vimmelskaftet");
+            vimmelskaftet.setTitle((String)StreetName.get(32));
             vimmelskaftet.setSubText("kr. 6.000");
             vimmelskaftet.setBorder(Color.BLACK);
             vimmelskaftet.setBackGroundColor(Color.yellow);
 
             GUI_Street nygade = new GUI_Street();
             fields[34] = nygade;
-            nygade.setTitle("Nygade");
+            nygade.setTitle((String)StreetName.get(34));
             nygade.setSubText("kr. 6.400");
             nygade.setBorder(Color.BLACK);
             nygade.setBackGroundColor(Color.yellow);
 
             GUI_Street frederiksberggade = new GUI_Street();
             fields[37] = frederiksberggade;
-            frederiksberggade.setTitle("Frederiksberggade");
+            frederiksberggade.setTitle((String)StreetName.get(37));
             frederiksberggade.setSubText("kr. 7.000");
             frederiksberggade.setBorder(Color.BLACK);
             frederiksberggade.setBackGroundColor(new Color(139, 0, 139));
 
             GUI_Street rådhuspladsen = new GUI_Street();
             fields[39] = rådhuspladsen;
-            rådhuspladsen.setTitle("Rådhuspladsen");
+            rådhuspladsen.setTitle((String)StreetName.get(39));
             rådhuspladsen.setSubText("kr. 8.000");
             rådhuspladsen.setBorder(Color.BLACK);
             rådhuspladsen.setBackGroundColor(new Color(139, 0, 139));
@@ -428,22 +429,22 @@ public class GUI_Handler {
             //færge felter
             GUI_Shipping lBfærgerne = new GUI_Shipping();
             fields[5] = lBfærgerne;
-            lBfærgerne.setTitle("LB færgerne");
+            lBfærgerne.setTitle((String)StreetName.get(5));
             lBfærgerne.setSubText("4.000");
 
             GUI_Shipping danmark = new GUI_Shipping();
             fields[15] = danmark;
-            danmark.setTitle("Danmark");
+            danmark.setTitle((String)StreetName.get(15));
             danmark.setSubText("4.000");
 
             GUI_Shipping mols_linien = new GUI_Shipping();
             fields[25] = mols_linien;
-            mols_linien.setTitle("Mols-linien");
+            mols_linien.setTitle((String)StreetName.get(25));
             mols_linien.setSubText("4.000");
 
             GUI_Shipping skandinavisk = new GUI_Shipping();
             fields[35] = skandinavisk;
-            skandinavisk.setTitle("Skandinavisk");
+            skandinavisk.setTitle((String)StreetName.get(35));
             skandinavisk.setSubText("4.000");
 
             //fængsel felt
@@ -458,12 +459,12 @@ public class GUI_Handler {
             //bryggeri felter
             GUI_Brewery carlsberg = new GUI_Brewery();
             fields[12] = carlsberg;
-            carlsberg.setTitle("Carlsberg");
+            carlsberg.setTitle((String)StreetName.get(12));
             carlsberg.setSubText("3.000");
 
             GUI_Field coca_Cola = new GUI_Street();
             fields[28] = coca_Cola;
-            coca_Cola.setTitle("Coca Cola");
+            coca_Cola.setTitle((String)StreetName.get(28));
             coca_Cola.setSubText("3.000");
             coca_Cola.setBackGroundColor(Color.red);
 
@@ -566,6 +567,11 @@ public class GUI_Handler {
 
         trading.trade(playerC, init, initOffer, initMoney,
                                rece, receOffer, receMoney);
+
+        updateOwnerBorders(init, receOffer);
+        updateOwnerBorders(rece, initOffer);
+
+        updateGuiPlayerBalance(playerC);
     }
 
     private String[] getTradeOwnable(PlayerController playerC, int ref){
@@ -591,5 +597,19 @@ public class GUI_Handler {
         } while(!selected.equals("AFSLUT"));
 
         return offer;
+    }
+
+    private void updateOwnerBorders(int ref, String[] offer){
+        GUI_Field[] fields = gui.getFields();
+        for (int fieldNum=0 ; fieldNum < fields.length ; fieldNum++) {
+            if (fields[fieldNum] instanceof GUI_Ownable){
+                for (int offerNum=0 ; offerNum < offer.length ; offerNum++) {
+                    if (fields[fieldNum].getTitle().equals(offer[offerNum])){
+                        GUI_Ownable owned = (GUI_Ownable)fields[fieldNum];
+                        owned.setBorder(getGuiPlayer(ref).getCar().getPrimaryColor());
+                    }
+                }
+            }
+        }
     }
 }
