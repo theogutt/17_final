caed54317063e2749cf95720293250298c81aa02
Andreas <mail.Tailspot@gmail.com>
2019-01-18 16:35:32 +0100

    Lavet kommentare til MessageHandler -Andreas

--- src/main/java/View/MessageHandler.java
+++ src/main/java/View/MessageHandler.java
@@ -13,21 +13,26 @@ public class MessageHandler {
     public MessageHandler() throws IOException {
         hashMapTurnMessages = (TextReader.textReader(".\\src\\Resources\\TurnMessages"));
     }
+
+    //Siger at en spiller får 4.000 kr. for lande eller passere start
     public String messageSquare(PlayerController playerC, int ref) {
         returnString = playerC.getName(ref) + getSquareMessage(0);
         return returnString;
     }
+
+    //Giver spillerens navn og en besked
     public String turnMessage(PlayerController playerC, int ref, String kindOfSquare) {
         return playerC.getName(ref) + getSquareMessage(Integer.parseInt(kindOfSquare));
     }
-    public String getSquareMessage(int index) {
-        return (String) hashMapTurnMessages.get(index);
-    }
+
+
+    //Siger at en spiller er gået fallit
     public String gotBroke(PlayerController playerC, int i) {
         returnString = turnMessage(playerC, i, "28");
         return returnString;
     }
 
+    //Disse forklare spillets regler
     public String startGame1() {
         return getSquareMessage(996);
     }
@@ -41,20 +46,32 @@ public class MessageHandler {
         return getSquareMessage(999);
     }
 
+
+    //Retunere en besked
     public String gameMessage(String messageNum){
         return getSquareMessage(Integer.parseInt(messageNum));
     }
 
+    //Siger hvilken spillers tur det
     public String playerTurn(PlayerController playerC, int i) {
         returnString = "Det er " + playerC.getName(i) + "'s tur! Tryk enter for at kaste terningerne!";
         return returnString;
     }
+
+    //Siger hvilken spiller, der har vundet
     public String playerWon(PlayerController playerC, int i) {
         returnString = playerC.getName(i) + " har vundet spillet! Med et beløb på " + playerC.getBalance(i);
         return returnString;
     }
+
+    //Fortæller at en spiller ikke ejen alle ejendomme af samme farve
     public String notAllID(){
         returnString = "Du ejer ikke alle felter i denne farve.";
         return returnString;
+    }
+
+    //Getter en besked fra hashmappet TurnMessages
+    public String getSquareMessage(int index) {
+        return (String) hashMapTurnMessages.get(index);
     }
 }
