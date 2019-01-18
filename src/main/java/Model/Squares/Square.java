ffb901b2c4e4669c2867626599a20c33bc6f3269
Andreas <mail.Tailspot@gmail.com>
2019-01-18 15:29:36 +0100

    Lavet kommentare til Start og Square -Andreas

--- src/main/java/Model/Squares/Square.java
+++ src/main/java/Model/Squares/Square.java
@@ -11,8 +11,10 @@ public abstract class Square{
         this.positionOnBoard = positionOnBoard;
     }
 
+    //Metode til hvad der skal ske når en spiller lander på et felt. Andre klasser nedarver denne medtode
     public void landOn(PlayerController playerC, int ref, GUI_Handler guiHandler, RentController rentC){ }
 
+    //Getter
     public int getPositionOnBoard(){
         return positionOnBoard;
     }
