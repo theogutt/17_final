a7190fca55f3f4d8b156d29955bd4578407cf2de
theogutt <theodorguttesen@gmail.com>
2019-01-18 11:12:29 +0100

    Er gået i gang med JUnit test

---
+++ src/main/java/Model/AccountTest.java
@@ -0,0 +1,30 @@
+package Model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AccountTest {
+    Account accountPlayers;
+
+    @Before
+    public void initialize(){
+        accountPlayers = new Account();
+    }
+
+    @Test
+    public void startAccount() {
+        assertEquals(30000, accountPlayers.getBalance());
+    }
+
+    @Test
+    public void updateBalance() {
+        assertEquals(30000, accountPlayers.getBalance());
+        accountPlayers.updateBalance(2050);
+        assertEquals(32050, accountPlayers.getBalance());
+        accountPlayers.updateBalance(-2050);
+        assertEquals(30000, accountPlayers.getBalance());
+    }
+
+}
