b638338bc37af44013cc3c6e2f1200c465ce7eb9
aleksanderlj <aleksanderlj@hotmail.com>
2019-01-19 12:43:14 +0100

    Fjernet start landon

--- src/main/java/Model/Squares/Start.java
+++ src/main/java/Model/Squares/Start.java
@@ -1,17 +1,8 @@
 package Model.Squares;
 
-import Controller.PlayerController;
-import Controller.RentController;
-import View.GUI_Handler;
-
 public class Start extends Square{
-
 
     public Start(int positionOnBoard){
         super(positionOnBoard);
-    }
-
-    //Giver spiller 4.000 kr.
-    public void landOn(PlayerController playerC, int playerNum, GUI_Handler guiHandler, RentController rentC){
     }
 }
